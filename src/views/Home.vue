0b3ef84b74a9a90e7f6cc4300723178b103951cd
kye <lz271253195@gamil.com>
2018-12-24 14:37:48 +0800

    reset

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-        heiheiheiya
     </div>
 </template>
 
