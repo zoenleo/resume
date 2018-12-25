cfcdedccd59bae2bf9b2ac30d32b32a1bd8455e9
zoenleo <lz271253196@gmail.com>
2018-12-25 19:56:32 +0800

    status

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="app">
         <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
+            <router-link to="/">我的简历</router-link> |
+            <router-link to="/about">个人简介</router-link>
         </div>
         <router-view />
     </div>
