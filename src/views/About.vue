c2e1b34b64f4504f67686178ee850b9a9ead6268
刘重 <maxliu@webank.com>
2022-11-10 19:56:07 +0800

    feat: 11月10日更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,10 @@
             熟悉html、js、css基础，并且能熟练使用html5、css3、es6、es7、typescript。
         </p>
         <p>
-            熟悉各大主流框架，使用过Jquery、zepto等传统型框架，也写过angular1、vue、react等mvc/mvvm框架项目，当前主要工作框架是react、vue。
+            熟悉各大主流框架，使用过Jquery、zepto等传统型框架，也写过angular、vue、react等mvc/mvvm框架项目，当前主要工作框架是react、vue。
         </p>
         <p>会写css预处理语言，常用less和sass。</p>
-        <p>使用webpack/gulp搭建项目，常常参与公司项目架构。</p>
+        <p>使用webpack/vite搭建项目，常常参与公司项目架构。</p>
         <p>
             写过PC网页、移动端、微信公众号、微信小程序、react-native
             APP、chrome浏览器插件，并且都有线上项目。
@@ -16,7 +16,7 @@
         <p>为微信小程序写过开源库，并在npm上发布。 了解命令行的常用操作。</p>
         <p>有自己的掘金博客，并且常在上面写一些文章。</p>
         <p>
-            拥有自己的vps服务器，做过基于爬虫的小说网站后来搁置，现在服务器上放的是写给女朋友的信和搭建科学上网工具。
+            拥有自己的vps服务器，做过基于爬虫的网站后来搁置，现作为科学上网工具。
         </p>
     </div>
 </template>
